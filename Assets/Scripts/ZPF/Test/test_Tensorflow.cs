4c805113c8d72765a99a64be36020bdaa423edac
Zeratul Phillip <ZhangPengfei@Zeratuls-Mac-mini.local>
2017-03-02 10:13:27 +0800

    result is nice

--- Assets/Scripts/ZPF/Test/test_Tensorflow.cs
+++ Assets/Scripts/ZPF/Test/test_Tensorflow.cs
@@ -14,13 +14,32 @@ public class test_Tensorflow : MonoBehaviour {
 
 		Mouse mouse = new Mouse(inputTexture);
 
-		Texture2D headTex = mouse.tail.texture;
-		Mat image = new Mat(headTex.height, headTex.width, CvType.CV_8UC3);
-		Utils.texture2DToMat(headTex, image);
+		Texture2D displayTex = mouse.head.texture;
+		GameObject.Find("Head").GetComponent<Renderer>().material.mainTexture = displayTex;
 
-		Texture2D displayTexture = new Texture2D(image.width(), image.height());
-		Utils.matToTexture2D(image, displayTexture);
-		gameObject.GetComponent<Renderer>().material.mainTexture = displayTexture;	
+		displayTex = mouse.leftEar.texture;
+		GameObject.Find("Left Ear").GetComponent<Renderer>().material.mainTexture = displayTex;
+
+		displayTex = mouse.rightEar.texture;
+		GameObject.Find("Right Ear").GetComponent<Renderer>().material.mainTexture = displayTex;
+
+		displayTex = mouse.body.texture;
+		GameObject.Find("Body").GetComponent<Renderer>().material.mainTexture = displayTex;
+
+		displayTex = mouse.leftArm.texture;
+		GameObject.Find("Left Arm").GetComponent<Renderer>().material.mainTexture = displayTex;
+
+		displayTex = mouse.rightArm.texture;
+		GameObject.Find("Right Arm").GetComponent<Renderer>().material.mainTexture = displayTex;
+
+		displayTex = mouse.leftLeg.texture;
+		GameObject.Find("Left Leg").GetComponent<Renderer>().material.mainTexture = displayTex;
+
+		displayTex = mouse.rightLeg.texture;
+		GameObject.Find("Right Leg").GetComponent<Renderer>().material.mainTexture = displayTex;
+	
+		displayTex = mouse.tail.texture;
+		GameObject.Find("Tail").GetComponent<Renderer>().material.mainTexture = displayTex;
 	}
 	
 	// Update is called once per frame
