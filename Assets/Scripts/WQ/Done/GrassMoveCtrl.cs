92b2715fb48db34296df834d53e4e7e87d5286b0
王茜 <wangqian@Wangqian.local>
2017-03-06 20:28:55 +0800

    add grassMoveCtrl script

---
+++ Assets/Scripts/WQ/Done/GrassMoveCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassMoveCtrl : MonoBehaviour 
+{
+	public float speed;
+	public Vector3 leftPos;//左边界点
+	public Vector3 rightPos;//右边界点
+	Vector3 offset;
+
+	public bool move;
+
+	void Start () 
+	{
+		offset=new Vector3(0.01f,0,0);
+		speed=3f;
+		move =true;
+	}
+	
+
+	void Update () 
+	{
+		if (move) 
+		{
+			transform.localPosition-=offset*speed;
+			if ((transform.localPosition-leftPos).x<=0.01f) 
+			{
+				transform.localPosition=rightPos;
+			}
+			
+		}
+
+	}
+
+
+}
