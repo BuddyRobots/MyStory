dea9a8866ee55c90c00ca3ed7d8fe3b1ce73e283
王茜 <wangqian@Wangqian.local>
2017-03-06 13:34:16 +0800

    delete useless test scenes and scripts

---
+++ Assets/Scripts/WQ/Test/BallMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//测试小球滚动
+
+public class BallMove : MonoBehaviour
+{
+	public GameObject mouse;
+	public float speed;
+
+	private Rigidbody2D rig2D;
+	private Rigidbody2D mouseRig2D;
+
+	void Start () 
+	{
+		rig2D =gameObject.GetComponent<Rigidbody2D>();
+		mouseRig2D=mouse.GetComponent<Rigidbody2D>();
+	}
+
+
+//	void FixedUpdate ()
+//	{
+//		if (Input.GetKeyDown(KeyCode.A)) 
+//		{
+//			Vector2 movement=new Vector2(Random.Range(1f,2f),Random.Range(2f,5f));
+//			rig2D.velocity=movement*speed;
+//		}
+//		if (Input.GetKeyDown(KeyCode.S)) 
+//		{
+//			Vector2 movement=new Vector2(-Random.Range(1f,2f),Random.Range(2f,5f));
+//			rig2D.velocity=movement*speed;
+//		}
+//
+//	}
+
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.name=="Mouse")
+		{
+			Vector2 movement=new Vector2(Random.Range(1f,2f),Random.Range(2f,5f));
+			rig2D.velocity=movement*speed;
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/WQ/Test/DragTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnDrag(Vector2 delta)
+	{
+		Debug .Log ("//////");
+		Ray ray = UICamera.current.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+		Plane plane = new Plane(transform.forward, transform.position);
+		float dist;
+		if (plane.Raycast(ray,out dist))
+		{
+			Vector3 worldPos = ray.GetPoint(dist);
+			transform.position = worldPos;
+		}
+
+
+	}
+}
