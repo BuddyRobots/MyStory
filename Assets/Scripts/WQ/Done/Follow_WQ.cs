85653e6954a79f2df4276779ed86cd747fa9fdce
王茜 <wangqian@Wangqian.local>
2017-03-10 17:39:18 +0800

    add black mask

---
+++ Assets/Scripts/WQ/Done/Follow_WQ.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[ExecuteInEditMode]
+public class Follow_WQ : MonoBehaviour
+{
+	public Transform target;
+	public Vector3 offset;
+
+	void LateUpdate()
+	{
+		if(target)
+		{
+			transform.position = new Vector3((target.position + offset).x,0,(target.position + offset).z);
+		}
+	}
+}
