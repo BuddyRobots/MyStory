0624acd9656c9a1c2f4e7c3e3f1c0920e400507e
Zeratul Phillip <ZhangPengfei@Zeratuls-Mac-mini.local>
2017-04-10 16:35:39 +0800

    adde finishedRecording flag

---
+++ Assets/Scripts/ZPF/MyUtils/ReleaseRenderTexture.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ReleaseRenderTexture : MonoBehaviour {
+
+	void OnDisable()
+	{		
+		if (GetComponent<Camera>().targetTexture)
+			GetComponent<Camera>().targetTexture.Release();
+	}
+}
