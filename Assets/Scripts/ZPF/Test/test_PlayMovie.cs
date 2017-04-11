8e09369a61ed88e402052d851805fabb7e2ee7a1
Zeratul Phillip <ZhangPengfei@Zeratuls-Mac-mini.local>
2017-04-11 15:12:52 +0800

    succeeded play movie

---
+++ Assets/Scripts/ZPF/Test/test_PlayMovie.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+
+public class test_PlayMovie : MonoBehaviour
+{
+	void Start ()
+	{
+		string moviePath = Path.Combine(Application.streamingAssetsPath, "Movies/IMG_1120.mov");
+
+		if (File.Exists(moviePath))
+			Debug.Log("movie exists.");
+		else
+			Debug.Log("movie does not exist.");
+
+		Handheld.PlayFullScreenMovie("file://" + moviePath, Color.black, FullScreenMovieControlMode.CancelOnInput);
+	}
+}
